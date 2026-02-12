ff470e0d489b02722e50eeb6da9430beddbb9315
tlyy0017 <tlyy0017@student.monash.edu>
2026-02-12 23:07:29 +1100

    task 5

---
+++ src/main/java/com/jpmc/midascore/controller/BalanceController.java
@@ -0,0 +1,30 @@
+package com.jpmc.midascore.controller;
+
+import java.util.Optional;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.jpmc.midascore.entity.UserRecord;
+import com.jpmc.midascore.foundation.Balance;
+import com.jpmc.midascore.repository.UserRepository;
+
+@RestController
+public class BalanceController {
+
+    private final UserRepository userRepository;
+
+    public BalanceController(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @GetMapping("/balance")
+    public Balance getBalance(@RequestParam Long userId) {
+        Optional<UserRecord> user = userRepository.findById(userId);
+        if (user.isEmpty()) {
+            return new Balance(0);
+        }
+        return new Balance(user.get().getBalance());
+    }
+}
